41a9c19a85b195ebd3a864deee4ff7fb5e8aec3d
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-12 22:47:16 -0300

    Atividade Medidas

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            double metros, centimetros, quilometros;
+            
+            Console.WriteLine("Entre com a medida (em metros):");
+            entrada = Console.ReadLine();
+
+            metros = Convert.ToDouble(entrada);
+            centimetros = metros * 100;
+            quilometros = metros / 1000;
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("---Equivalência das medidas---");
+            Console.ResetColor();
+            Console.WriteLine($"{metros:N2} metros");
+            Console.WriteLine($"{centimetros} cm");
+            Console.WriteLine($"{quilometros:N3} km");
+
+            
+            
+        }
+    }
+}
